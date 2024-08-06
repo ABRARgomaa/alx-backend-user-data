1cbb6e881a42a038ebc5cbd0a3fcb8347f525ea4
ABRARgomaa <abrargomaa111@gmail.com>
2024-08-07 02:19:19 +0300

    up

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,32 @@
+from flask import request
+from typing import List, TypeVar
+
+User = TypeVar('User')
+
+
+class Auth:
+    """auth class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Determines if the given path requires authentication.
+        :param path: The path to check
+        :param excluded_paths:list of paths not require authentication
+        :return: False (default implementation)
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Retrieves the authorization header from the request.
+        :param request: The request object
+        :return: None (default implementation)
+        """
+        return None
+
+    def current_user(self, request=None) -> User:
+        """
+        Retrieves the current user from the request.
+        :param request: The request object
+        :return: None (default implementation)
+        """
+        return None
